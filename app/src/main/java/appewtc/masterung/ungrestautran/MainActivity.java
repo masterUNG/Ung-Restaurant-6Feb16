8b9b84ff786b82231325d3c0d0f45719285b4242
masterUNG <phrombutr@gmail.com>
2016-02-06 12:42:16 +0700

    Create SQLite เสร็จแว้ววว

--- app/src/main/java/appewtc/masterung/ungrestautran/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ungrestautran/MainActivity.java
@@ -5,10 +5,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit
+    private MyManage objMyManage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Request Database
+        objMyManage = new MyManage(this);
+
+    }   // Main Method
+
+}   // Main Class
---
+++ app/src/main/java/appewtc/masterung/ungrestautran/MyManage.java
@@ -0,0 +1,24 @@
+package appewtc.masterung.ungrestautran;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by masterUNG on 2/6/16 AD.
+ */
+public class MyManage {
+
+    //Explicit
+    private MyOpenHelper objMyOpenHelper;
+    private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
+
+    public MyManage(Context context) {
+
+        //Create & Connected Database
+        objMyOpenHelper = new MyOpenHelper(context);
+        writeSqLiteDatabase = objMyOpenHelper.getWritableDatabase();
+        readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
+
+    }   // Constructor
+
+}   // Main Class
