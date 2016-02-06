92fdbcbb2fb96910eb9fbf6b5998afe1cc5cb282
masterUNG <phrombutr@gmail.com>
2016-02-06 15:05:07 +0700

    Finish 6Feb16

--- app/src/main/java/appewtc/masterung/ungrestautran/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ungrestautran/MainActivity.java
@@ -1,8 +1,12 @@
 package appewtc.masterung.ungrestautran;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,8 +28,45 @@ public class MainActivity extends AppCompatActivity {
         //Clean data
         cleanData();
 
+        //Synchronize JSON to SQLite
+        synJSONtoSQLite();
+
 
     }   // Main Method
+
+    private void synJSONtoSQLite() {
+
+        //Change Policy
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myPolicy);
+
+        int intTable = 1;
+        String tag = "Restaurant";
+
+        while (intTable <= 2) {
+
+            //1. Create InputStream
+            InputStream objInputStream = null;
+            String strURLuser = "http://swiftcodingthai.com/6feb/php_get_data_master.php";
+            String strURLfood = "http://swiftcodingthai.com/6feb/php_get_data_food.php";
+
+            try {
+
+            } catch (Exception e) {
+                Log.d(tag, "InputStream ==> " + e.toString());
+            }
+
+
+            //2. Create JSON String
+
+            //3. Update SQLite
+
+
+
+            intTable += 1;
+        }   // while
+    }   // synJSONtoSQLite
 
     private void cleanData() {
 
