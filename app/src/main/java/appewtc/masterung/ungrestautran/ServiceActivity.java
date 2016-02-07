5e7b696130e79ccb020b46d90d34d6d8f2beae16
masterUNG <phrombutr@gmail.com>
2016-02-07 15:45:50 +0700

    Project Finish

--- app/src/main/java/appewtc/masterung/ungrestautran/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/ungrestautran/ServiceActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,16 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -87,11 +98,42 @@ public class ServiceActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialogInterface, int i) {
                 amountString = Integer.toString(i + 1);
                 dialogInterface.dismiss();
+                updateOrder();
             }
         });
         objBuilder.show();
 
     }   // confirmOrder
+
+    private void updateOrder() {
+
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myPolicy);
+
+        try {
+
+            ArrayList<NameValuePair> objNameValuePairs = new ArrayList<NameValuePair>();
+            objNameValuePairs.add(new BasicNameValuePair("isAdd", "true"));
+            objNameValuePairs.add(new BasicNameValuePair("Officer", officerString));
+            objNameValuePairs.add(new BasicNameValuePair("Desk", deskString));
+            objNameValuePairs.add(new BasicNameValuePair("Food", myfoodString));
+            objNameValuePairs.add(new BasicNameValuePair("Item", amountString));
+
+            HttpClient objHttpClient = new DefaultHttpClient();
+            HttpPost objHttpPost = new HttpPost("http://swiftcodingthai.com/6feb/php_add_data.php");
+            objHttpPost.setEntity(new UrlEncodedFormEntity(objNameValuePairs, "UTF-8"));
+            objHttpClient.execute(objHttpPost);
+
+            Toast.makeText(ServiceActivity.this,
+                    "Update Success", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Toast.makeText(ServiceActivity.this,
+                    "Cannot Update", Toast.LENGTH_SHORT).show();
+        }
+
+    }   // updateOrder
 
     private void showDesk() {
 
